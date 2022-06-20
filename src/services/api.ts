7d71588b1aec96162e923c67fd73dc544a34db38
Guilherme Lourenço da Silva <guilhermesilva@acad.ifma.edu.br>
2022-06-20 10:23:46 -0300

    tsconfig fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -26,7 +26,7 @@ export function setupAPIClient(ctx = undefined) {
       return response;
     },
     (error: AxiosError) => {
-      if (error.response.status === 401) {
+      if (error.response?.status === 401) {
         const { message } = error.response.data as ErrorResponse;
 
         if (message === 'Token expired!') {
